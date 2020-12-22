4b29414190df11eb075843d1f80728244d36729f
nnaudot <nnaudot@manzalab.com>
2020-12-22 10:21:24 +0100

    Add part 2 of day 22 of 2020

--- AdventOfCode/Year2020/DayTwentyTwo.cs
+++ AdventOfCode/Year2020/DayTwentyTwo.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode.Year2020
 {
@@ -7,6 +9,7 @@ namespace AdventOfCode.Year2020
 	{
 		private Queue<int> mFirstPlayerCards = new Queue<int>();
 		private Queue<int> mSecondPlayerCards = new Queue<int>();
+		private int mCurrentGame = 1;
 
 		protected override object ResolveFirstPart()
 		{
@@ -79,10 +82,11 @@ namespace AdventOfCode.Year2020
 
 		protected override object ResolveSecondPart()
 		{
+			List<Queue<int>> firstPlayerUsedDecks = new List<Queue<int>>();
+			List<Queue<int>> secondPlayerUsedDecks = new List<Queue<int>>();
+
 			mFirstPlayerCards.Clear();
 			mSecondPlayerCards.Clear();
-
-			// Before either player deals a card, if there was a previous round in this game that had exactly the same cards in the same order in the same players' decks, the game instantly ends in a win for player 1. Previous rounds from other games are not considered. (This prevents infinite games of Recursive Combat, which everyone agrees is a bad idea.)
 
 			string[] input = File.ReadAllLines(GetResourcesPath());
 
@@ -113,26 +117,106 @@ namespace AdventOfCode.Year2020
 				}
 			}
 
+			int currentRound = 1;
+
+			// We keep the first game here
 			while (mFirstPlayerCards.Count != 0 && mSecondPlayerCards.Count != 0)
 			{
 				int firstPlayerValue = mFirstPlayerCards.Dequeue();
 				int secondPlayerValue = mSecondPlayerCards.Dequeue();
 
-				if (firstPlayerValue > secondPlayerValue)
+				//Console.WriteLine("Round " + currentRound + " of Game " + mCurrentGame);
+				//Console.WriteLine("Player 1 plays " + firstPlayerValue);
+				//Console.WriteLine("Player 2 plays " + secondPlayerValue);
+
+				if (firstPlayerValue <= mFirstPlayerCards.Count && secondPlayerValue <= mSecondPlayerCards.Count)
+				{
+					Queue<int> firstPlayerNewQueue = new Queue<int>();
+					for (int i = 0; i < firstPlayerValue; i++)
+					{
+						if (i >= mFirstPlayerCards.Count)
+						{
+							break;
+						}
+
+						firstPlayerNewQueue.Enqueue(mFirstPlayerCards.ElementAt(i));
+					}
+					Queue<int> secondPlayerNewQueue = new Queue<int>();
+					for (int i = 0; i < secondPlayerValue; i++)
+					{
+						if (i >= mSecondPlayerCards.Count)
+						{
+							break;
+						}
+
+						secondPlayerNewQueue.Enqueue(mSecondPlayerCards.ElementAt(i));
+					}
+
+					bool isFirstPlayerWin = PlayRound(firstPlayerNewQueue, secondPlayerNewQueue);
+					if (isFirstPlayerWin)
+					{
+						mFirstPlayerCards.Enqueue(firstPlayerValue);
+						mFirstPlayerCards.Enqueue(secondPlayerValue);
+
+						if (CheckInfinite(firstPlayerUsedDecks, mFirstPlayerCards))
+						{
+							break;
+						}
+						firstPlayerUsedDecks.Add(new Queue<int>(mFirstPlayerCards));
+					}
+					else
+					{
+						mSecondPlayerCards.Enqueue(secondPlayerValue);
+						mSecondPlayerCards.Enqueue(firstPlayerValue);
+
+						if (CheckInfinite(secondPlayerUsedDecks, mSecondPlayerCards))
+						{
+							break;
+						}
+						secondPlayerUsedDecks.Add(new Queue<int>(mSecondPlayerCards));
+					}
+				}
+				else if (firstPlayerValue > secondPlayerValue)
 				{
 					mFirstPlayerCards.Enqueue(firstPlayerValue);
 					mFirstPlayerCards.Enqueue(secondPlayerValue);
+
+					if (CheckInfinite(firstPlayerUsedDecks, mFirstPlayerCards))
+					{
+						break;
+					}
+					firstPlayerUsedDecks.Add(new Queue<int>(mFirstPlayerCards));
 				}
 				else if (firstPlayerValue < secondPlayerValue)
 				{
 					mSecondPlayerCards.Enqueue(secondPlayerValue);
 					mSecondPlayerCards.Enqueue(firstPlayerValue);
+
+					if (CheckInfinite(secondPlayerUsedDecks, mSecondPlayerCards))
+					{
+						break;
+					}
+					secondPlayerUsedDecks.Add(new Queue<int>(mSecondPlayerCards));
 				}
 				else
 				{
 					mFirstPlayerCards.Enqueue(firstPlayerValue);
 					mSecondPlayerCards.Enqueue(secondPlayerValue);
+
+					if (CheckInfinite(firstPlayerUsedDecks, mFirstPlayerCards))
+					{
+						break;
+					}
+					firstPlayerUsedDecks.Add(new Queue<int>(mFirstPlayerCards));
+
+					if (CheckInfinite(secondPlayerUsedDecks, mSecondPlayerCards))
+					{
+						break;
+					}
+					secondPlayerUsedDecks.Add(new Queue<int>(mSecondPlayerCards));
 				}
+
+				currentRound++;
 			}
 
 			Queue<int> usedQueue = mFirstPlayerCards.Count == 0 ? mSecondPlayerCards : mFirstPlayerCards;
@@ -146,6 +230,157 @@ namespace AdventOfCode.Year2020
 			}
 
 			return result;
+		}
+
+		/// <summary>
+		/// Returns true if the first player wins.
+		/// </summary>
+		private bool PlayRound(Queue<int> firstPlayerCards, Queue<int> secondPlayerCards) // TODO : Faire une copie des queues
+		{
+			List<Queue<int>> firstPlayerUsedDecks = new List<Queue<int>>();
+			List<Queue<int>> secondPlayerUsedDecks = new List<Queue<int>>();
+
+			mCurrentGame++;
+
+			firstPlayerUsedDecks.Add(new Queue<int>(firstPlayerCards));
+			secondPlayerUsedDecks.Add(new Queue<int>(secondPlayerCards));
+
+			int currentRound = 1;
+
+			while (firstPlayerCards.Count != 0 && secondPlayerCards.Count != 0)
+			{
+				int firstPlayerValue = firstPlayerCards.Dequeue();
+				int secondPlayerValue = secondPlayerCards.Dequeue();
+
+				//Console.WriteLine("Round " + currentRound + " of Game " + mCurrentGame);
+				//Console.WriteLine("Player 1 plays " + firstPlayerValue);
+				//Console.WriteLine("Player 2 plays " + secondPlayerValue);
+
+				if (firstPlayerValue <= firstPlayerCards.Count && secondPlayerValue <= secondPlayerCards.Count)
+				{
+					Queue<int> firstPlayerNewQueue = new Queue<int>();
+					for (int i = 0; i < firstPlayerValue; i++)
+					{
+						firstPlayerNewQueue.Enqueue(firstPlayerCards.ElementAt(i));
+					}
+					Queue<int> secondPlayerNewQueue = new Queue<int>();
+					for (int i = 0; i < secondPlayerValue; i++)
+					{
+						secondPlayerNewQueue.Enqueue(secondPlayerCards.ElementAt(i));
+					}
+
+					bool isFirstPlayerWin = PlayRound(firstPlayerNewQueue, secondPlayerNewQueue);
+					if (isFirstPlayerWin)
+					{
+						firstPlayerCards.Enqueue(firstPlayerValue);
+						firstPlayerCards.Enqueue(secondPlayerValue);
+
+						if (CheckInfinite(firstPlayerUsedDecks, firstPlayerCards))
+						{
+							mCurrentGame--;
+							return true;
+						}
+						firstPlayerUsedDecks.Add(new Queue<int>(firstPlayerCards));
+					}
+					else
+					{
+						secondPlayerCards.Enqueue(secondPlayerValue);
+						secondPlayerCards.Enqueue(firstPlayerValue);
+
+						if (CheckInfinite(secondPlayerUsedDecks, secondPlayerCards))
+						{
+							mCurrentGame--;
+							return true;
+						}
+						secondPlayerUsedDecks.Add(new Queue<int>(secondPlayerCards));
+					}
+				}
+				else if (firstPlayerValue > secondPlayerValue)
+				{
+					firstPlayerCards.Enqueue(firstPlayerValue);
+					firstPlayerCards.Enqueue(secondPlayerValue);
+
+					if (CheckInfinite(firstPlayerUsedDecks, firstPlayerCards))
+					{
+						mCurrentGame--;
+						return true;
+					}
+					firstPlayerUsedDecks.Add(new Queue<int>(firstPlayerCards));
+				}
+				else if (firstPlayerValue < secondPlayerValue)
+				{
+					secondPlayerCards.Enqueue(secondPlayerValue);
+					secondPlayerCards.Enqueue(firstPlayerValue);
+
+					if (CheckInfinite(secondPlayerUsedDecks, secondPlayerCards))
+					{
+						mCurrentGame--;
+						return true;
+					}
+					secondPlayerUsedDecks.Add(new Queue<int>(secondPlayerCards));
+				}
+				else
+				{
+					firstPlayerCards.Enqueue(firstPlayerValue);
+					secondPlayerCards.Enqueue(secondPlayerValue);
+
+					if (CheckInfinite(firstPlayerUsedDecks, firstPlayerCards))
+					{
+						mCurrentGame--;
+						return true;
+					}
+					firstPlayerUsedDecks.Add(new Queue<int>(firstPlayerCards));
+
+					if (CheckInfinite(secondPlayerUsedDecks, secondPlayerCards))
+					{
+						mCurrentGame--;
+						return true;
+					}
+					secondPlayerUsedDecks.Add(new Queue<int>(secondPlayerCards));
+				}
+
+				currentRound++;
+			}
+
+			mCurrentGame--;
+			return firstPlayerCards.Count != 0;
+		}
+
+		/// <summary>
+		/// Returns true if infinite was found
+		/// </summary>
+		private bool CheckInfinite(List<Queue<int>> queues, Queue<int> newQueue)
+		{
+			// Verifying infinit recursion
+			for (int i = 0; i < queues.Count; i++)
+			{
+				if (queues[i].Count != newQueue.Count)
+				{
+					continue;
+				}
+
+				bool areEqual = true;
+
+				for (int j = 0; j < queues[i].Count; j++)
+				{
+					if (queues[i].ElementAt(j) != newQueue.ElementAt(j))
+					{
+						areEqual = false;
+						break;
+					}
+				}
+
+				if (areEqual)
+				{
+					return true;
+				}
+				//if (queues[i].Equals(newQueue))
+				//{
+				//	return true;
+				//}
+			}
+
+			return false;
 		}
 	}
 }
