3b4d9e706ee6cf73e590670d06c5f0cc71177abe
nnaudot <nnaudot@manzalab.com>
2020-12-24 10:37:49 +0100

    Add part 2 of day 24 of 2020

--- AdventOfCode/Year2020/DayTwentyFour.cs
+++ AdventOfCode/Year2020/DayTwentyFour.cs
@@ -7,6 +7,9 @@ namespace AdventOfCode.Year2020
 {
 	public class DayTwentyFour : Day2020
 	{
+		private List<Tile> mTiles = new List<Tile>();
+		private List<Tile> mNewTilesToProcess = new List<Tile>();
+
 		public class Tile
 		{
 			public int East;
@@ -16,9 +19,9 @@ namespace AdventOfCode.Year2020
 
 		protected override object ResolveFirstPart()
 		{
-			string[] input = File.ReadAllLines(GetResourcesPath());
+			mTiles.Clear();
 
-			List<Tile> tiles = new List<Tile>();
+			string[] input = File.ReadAllLines(GetResourcesPath());
 
 			for (int i = 0; i < input.Length; i++)
 			{
@@ -81,14 +84,14 @@ namespace AdventOfCode.Year2020
 					}
 				}
 
-				Tile alreadyUsed = tiles.FirstOrDefault(tile =>
+				Tile alreadyUsed = mTiles.FirstOrDefault(tile =>
 					tile.East == newTile.East
 					&& tile.South == newTile.South
 				);
 
 				if (alreadyUsed == null)
 				{
-					tiles.Add(newTile);
+					mTiles.Add(newTile);
 					newTile.IsWhite = !newTile.IsWhite;
 				}
 				else
@@ -97,12 +100,113 @@ namespace AdventOfCode.Year2020
 				}
 			}
 
-			return tiles.Where(tile => !tile.IsWhite).Count();
+			return mTiles.Where(tile => !tile.IsWhite).Count();
 		}
 
 		protected override object ResolveSecondPart()
 		{
-			return string.Empty;
+			ResolveFirstPart();
+
+			List<Tile> tilesToReplace = new List<Tile>();
+
+			for (int i = 0; i < 100; i++)
+			{
+				mNewTilesToProcess.Clear();
+				tilesToReplace.Clear();
+
+				for (int j = 0; j < mTiles.Count; j++)
+				{
+					Tile tile = mTiles[j];
+					int blackAdjacentTiles = CountBlackAdjacentTiles(tile);
+
+					if (tile.IsWhite)
+					{
+						if (blackAdjacentTiles == 2)
+						{
+							tilesToReplace.Add(new Tile() { East = tile.East, South = tile.South, IsWhite = false });
+						}
+					}
+					else
+					{
+						if (blackAdjacentTiles == 0 || blackAdjacentTiles > 2)
+						{
+							tilesToReplace.Add(new Tile() { East = tile.East, South = tile.South, IsWhite = true });
+						}
+					}
+				}
+
+				for (int j = 0; j < mNewTilesToProcess.Count; j++)
+				{
+					Tile tile = mNewTilesToProcess[j];
+					int blackAdjacentTiles = CountBlackAdjacentTiles(tile);
+
+					if (tile.IsWhite)
+					{
+						if (blackAdjacentTiles == 2)
+						{
+							tilesToReplace.Add(new Tile() { East = tile.East, South = tile.South, IsWhite = false });
+						}
+					}
+					else
+					{
+						if (blackAdjacentTiles == 0 || blackAdjacentTiles > 2)
+						{
+							tilesToReplace.Add(new Tile() { East = tile.East, South = tile.South, IsWhite = true });
+						}
+					}
+
+					mTiles.Add(tile);
+				}
+
+				for (int j = 0; j < tilesToReplace.Count; j++)
+				{
+					Tile toUpdate = mTiles.FirstOrDefault(savedTile => savedTile.East == tilesToReplace[j].East && savedTile.South == tilesToReplace[j].South);
+					toUpdate.IsWhite = tilesToReplace[j].IsWhite;
+				}
+
+				Console.WriteLine("Day " + (i + 1) + ": " + mTiles.Where(tile => !tile.IsWhite).Count());
+			}
+
+			return mTiles.Where(tile => !tile.IsWhite).Count();
+		}
+
+		private int CountBlackAdjacentTiles(Tile tile)
+		{
+			int blackAdjacentTile = 0;
+
+			blackAdjacentTile += ProcessTile(tile, 2, 0) ? 0 : 1; // East
+			blackAdjacentTile += ProcessTile(tile, 1, 1) ? 0 : 1; // SouthEast
+			blackAdjacentTile += ProcessTile(tile, -1, 1) ? 0 : 1; // SouthWest
+			blackAdjacentTile += ProcessTile(tile, -2, 0) ? 0 : 1; // West
+			blackAdjacentTile += ProcessTile(tile, -1, -1) ? 0 : 1; // NorthWest
+			blackAdjacentTile += ProcessTile(tile, 1, -1) ? 0 : 1; // NorthEast
+
+			return blackAdjacentTile;
+		}
+
+		private bool ProcessTile(Tile existingTile, int eastOffset, int southOffset)
+		{
+			// Look for a known tile
+			Tile tile = mTiles.FirstOrDefault(savedTile => savedTile.East == existingTile.East + eastOffset && savedTile.South == existingTile.South + southOffset);
+			
+			// If the tile is unknown
+			if (tile == null)
+			{
+				// We create it
+				tile = new Tile() { East = existingTile.East + eastOffset, South = existingTile.South + southOffset, IsWhite = true };
+
+				// If our existing tile is black
+				if (!existingTile.IsWhite)
+				{
+					if (mNewTilesToProcess.FirstOrDefault(createdTile => createdTile.East == tile.East && createdTile.South == tile.South) == null)
+					{
+						mNewTilesToProcess.Add(tile);
+					}
+				}
+			}
+
+			// Return the check
+			return tile.IsWhite;
 		}
 	}
 }
