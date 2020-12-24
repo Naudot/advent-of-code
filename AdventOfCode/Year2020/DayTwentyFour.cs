5857837106c93f701141840650a98f0f9fff66af
nnaudot <nnaudot@manzalab.com>
2020-12-24 08:48:15 +0100

    Add first part of day 24

--- AdventOfCode/Year2020/DayTwentyFour.cs
+++ AdventOfCode/Year2020/DayTwentyFour.cs
@@ -1,10 +1,103 @@
-﻿namespace AdventOfCode.Year2020
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Year2020
 {
 	public class DayTwentyFour : Day2020
 	{
+		public class Tile
+		{
+			public int East;
+			public int South;
+			public bool IsWhite = true;
+		}
+
 		protected override object ResolveFirstPart()
 		{
-			return string.Empty;
+			string[] input = File.ReadAllLines(GetResourcesPath());
+
+			List<Tile> tiles = new List<Tile>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				Tile newTile = new Tile();
+				string moves = input[i];
+
+				for (int j = 0; j < moves.Length; j++)
+				{
+					char firstChar = moves[j];
+
+					if (firstChar == 'e')
+					{
+						newTile.East += 2;
+					}
+					if (firstChar == 'w')
+					{
+						newTile.East -= 2;
+					}
+
+					if (j == moves.Length - 1)
+					{
+						continue;
+					}
+
+					char secondChar = moves[j + 1];
+					bool isSecondCharUsed = false;
+
+					if (firstChar == 's')
+					{
+						isSecondCharUsed = true;
+						if (secondChar == 'e')
+						{
+							newTile.South += 1;
+							newTile.East += 1;
+						}
+						else if (secondChar == 'w')
+						{
+							newTile.South += 1;
+							newTile.East -= 1;
+						}
+					}
+					if (firstChar == 'n')
+					{
+						isSecondCharUsed = true;
+						if (secondChar == 'e')
+						{
+							newTile.South -= 1;
+							newTile.East += 1;
+						}
+						else if (secondChar == 'w')
+						{
+							newTile.South -= 1;
+							newTile.East -= 1;
+						}
+					}
+
+					if (isSecondCharUsed)
+					{
+						j++;
+					}
+				}
+
+				Tile alreadyUsed = tiles.FirstOrDefault(tile =>
+					tile.East == newTile.East
+					&& tile.South == newTile.South
+				);
+
+				if (alreadyUsed == null)
+				{
+					tiles.Add(newTile);
+					newTile.IsWhite = !newTile.IsWhite;
+				}
+				else
+				{
+					alreadyUsed.IsWhite = !alreadyUsed.IsWhite;
+				}
+			}
+
+			return tiles.Where(tile => !tile.IsWhite).Count();
 		}
 
 		protected override object ResolveSecondPart()
