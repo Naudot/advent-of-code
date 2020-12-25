14ab6c407dd750f16b1ddc5c5ccceddb6ef6b29a
nnaudot <nnaudot@manzalab.com>
2020-12-25 14:37:42 +0100

    Finish the day 25 and the aoc 2020

--- AdventOfCode/Year2020/DayTwentyFive.cs
+++ AdventOfCode/Year2020/DayTwentyFive.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace AdventOfCode.Year2020
 {
 	public class DayTwentyFive : Day2020
 	{
-		private const int MAXIMUM_LOOP_SIZE = 100;
+		private const int SUBJECT_NUMBER = 7;
+		private const int MODULO_NUMBER = 20201227;
 
 		protected override object ResolveFirstPart()
 		{
@@ -13,67 +13,44 @@ namespace AdventOfCode.Year2020
 			ulong cardPublicKey = ulong.Parse(input[0]);
 			ulong doorPublicKey = ulong.Parse(input[1]);
 
-			// Exemple
-			// ulong exempleCardPublicKey = ProcessSubjectNumber(7, 8);
-			// ulong exempleDoorublicKey = ProcessSubjectNumber(7, 11);
+			int currentLoopSize = 0;
+			int doorLoopSizeTest = 0;
+			int cardLoopSizeTest = 0;
+			ulong currentValue = 1;
+			while (doorLoopSizeTest == 0 || cardLoopSizeTest == 0)
+			{
+				currentLoopSize++;
+				currentValue *= SUBJECT_NUMBER;
+				currentValue %= MODULO_NUMBER;
 
-			Console.WriteLine("Exemple public keys " + ProcessSubjectNumber(7, 8) + " - " + ProcessSubjectNumber(7, 11));
-			Console.WriteLine("Exemple encryption keys " + ProcessSubjectNumber(5764801, 11) + " - " + ProcessSubjectNumber(17807724, 8));
+				if (currentValue == cardPublicKey)
+				{
+					cardLoopSizeTest = currentLoopSize;
+				}
+				else if (currentValue == doorPublicKey)
+				{
+					doorLoopSizeTest = currentLoopSize;
+				}
 
-			// Calls that determined precomputed results
+				if (doorLoopSizeTest != 0 && cardLoopSizeTest != 0)
+				{
+					break;
+				}
+			}
 
-			//ulong cardSubjectNumber = 0;
-			//int cardLoopSize = 0;
-			//LookForValue(cardPublicKey, out cardSubjectNumber, out cardLoopSize);
-
-			//ulong doorSubjectNumber = 0;
-			//int doorLoopSize = 0;
-			//LookForValue(doorPublicKey, out doorSubjectNumber, out doorLoopSize);
-
-			// Precomputed results
-			// TODO : The subject number must be the same
-			ulong cardSubjectNumber = 26810;
-			int cardLoopSize = 24;
-			ulong doorSubjectNumber = 209765;
-			int doorLoopSize = 4;
-
-			Console.WriteLine("Public keys " + ProcessSubjectNumber(doorSubjectNumber, doorLoopSize) + " - " + ProcessSubjectNumber(cardSubjectNumber, cardLoopSize));
-			Console.WriteLine("Encryption keys " + ProcessSubjectNumber(cardPublicKey, doorLoopSize) + " - " + ProcessSubjectNumber(doorPublicKey, cardLoopSize));
-
-			return ProcessSubjectNumber(17807724, 8);
+			return ProcessSubjectNumber(cardPublicKey, doorLoopSizeTest);
 		}
 
 		protected override object ResolveSecondPart()
 		{
-			return string.Empty;
-		}
-
-		private void LookForValue(ulong lookedValue, out ulong subjectNumber, out int loopSize)
-		{
-			ulong currentValue = 0;
-			subjectNumber = 0;
-			loopSize = 0;
-
-			while (currentValue != lookedValue)
-			{
-				subjectNumber++;
-
-				loopSize = 0;
-				for (int i = 0; i < MAXIMUM_LOOP_SIZE; i++)
-				{
-					loopSize++;
-					currentValue = ProcessSubjectNumber(subjectNumber, loopSize);
-
-					if (currentValue == lookedValue)
-					{
-						break;
-					}
-				}
-			}
+			return "gg ez";
 		}
 
 		private ulong ProcessSubjectNumber(ulong subjectNumber, int loopSize)
 		{
+			// Value entre 0 et 20201227 pour les deux public keys
+			//
+
 			ulong value = 1;
 
 			for (int i = 0; i < loopSize; i++)
