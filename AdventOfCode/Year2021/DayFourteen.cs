9fd8634a954a1e32c7dcad8df3f07804d4ad6f50
Nicolas Naudot <nnaudot@manzalab.com>
2021-12-14 10:21:38 +0100

    Finish day fourteen

--- AdventOfCode/Year2021/DayFourteen.cs
+++ AdventOfCode/Year2021/DayFourteen.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -9,83 +8,19 @@ namespace AdventOfCode.Year2021
 	{
 		protected override object ResolveFirstPart(string[] input)
 		{
-			List<int> chain = new List<int>();
-			Dictionary<(int, int), int> pairs = new Dictionary<(int, int), int>();
-			Dictionary<int, int> charCount = new Dictionary<int, int>();
-
-			for (int i = 0; i < input[0].Length; i++)
-			{
-				chain.Add(input[0][i]);
-				if (charCount.ContainsKey(input[0][i]))
-				{
-					charCount[input[0][i]]++;
-				}
-				else
-				{
-					charCount.Add(input[0][i], 1);
-				}
-			}
-
-
-			for (int i = 2; i < input.Length; i++)
-			{
-				string line = input[i];
-				Match match = Regex.Match(line, @"(\w+) -> (\w)");
-				(int, int) pair = (match.Groups[1].Value[0], match.Groups[1].Value[1]);
-				int val = match.Groups[2].Value[0];
-				pairs.Add(pair, val);
-			}
-
-			int step = 10;
-			for (int i = 0; i < step; i++)
-			{
-				//Console.WriteLine();
-				//for (int j = 0; j < chain.Count; j++)
-				//{
-				//	Console.Write((char)chain[j]);
-				//}
-				int count = chain.Count;
-				for (int j = 0; j < count - 1; j++)
-				{
-					(int, int) pair = (chain[j], chain[j + 1]);
-					int valueToInsert = pairs[pair];
-					chain.Insert(j + 1, valueToInsert);
-					if (charCount.ContainsKey(valueToInsert))
-					{
-						charCount[valueToInsert]++;
-					}
-					else
-					{
-						charCount.Add(valueToInsert, 1);
-					}
-					j++;
-					count++;
-				}
-			}
-
-			int min = int.MaxValue;
-			int max = int.MinValue;
-
-			foreach (KeyValuePair<int, int> item in charCount)
-			{
-				if (item.Value < min)
-				{
-					min = item.Value;
-				}
-				if (item.Value > max)
-				{
-					max = item.Value;
-				}
-			}
-
-			return max - min;
+			return Resolve(input, 10);
 		}
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			Dictionary<(int, int), int> chain = new Dictionary<(int, int), int>();
+			return Resolve(input, 40);
+		}
+
+		private long Resolve(string[] input, int step)
+		{
+			Dictionary<(int, int), long> chain = new Dictionary<(int, int), long>();
 			Dictionary<(int, int), int> pairs = new Dictionary<(int, int), int>();
-			Dictionary<int, int> charCount = new Dictionary<int, int>();
+			Dictionary<int, long> charCount = new Dictionary<int, long>();
 
 			for (int i = 0; i < input[0].Length; i++)
 			{
@@ -122,11 +57,10 @@ namespace AdventOfCode.Year2021
 				pairs.Add(pair, val);
 			}
 
-			int step = 3;
 			for (int i = 0; i < step; i++)
 			{
-				Dictionary<(int, int), int> clone = chain.ToDictionary(entry => entry.Key, entry => entry.Value);
-				foreach (KeyValuePair<(int, int), int> clonedItem in clone)
+				Dictionary<(int, int), long> clone = chain.ToDictionary(entry => entry.Key, entry => entry.Value);
+				foreach (KeyValuePair<(int, int), long> clonedItem in clone)
 				{
 					if (clonedItem.Value == 0)
 					{
@@ -155,7 +89,7 @@ namespace AdventOfCode.Year2021
 					{
 						chain.Add(newSecondPair, clonedItem.Value);
 					}
-					chain[oldPair]--;
+					chain[oldPair] -= clonedItem.Value;
 
 					if (charCount.ContainsKey(valueToInsert))
 					{
@@ -168,10 +102,10 @@ namespace AdventOfCode.Year2021
 				}
 			}
 
-			int min = int.MaxValue;
-			int max = int.MinValue;
+			long min = long.MaxValue;
+			long max = long.MinValue;
 
-			foreach (KeyValuePair<int, int> item in charCount)
+			foreach (KeyValuePair<int, long> item in charCount)
 			{
 				if (item.Value < min)
 				{
