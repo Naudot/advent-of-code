004913c7d9c4d3f91ce077907670e0bd3ef8774e
nnaudot <nnaudot@manzalab.com>
2022-12-11 18:47:06 +0100

    Begin day five

---
+++ AdventOfCode/Year2022/DayFive.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Year2022
+{
+	public class DayFive : Day2022
+	{
+		protected override object ResolveFirstPart(string[] input)
+		{
+			return string.Empty;
+		}
+
+		protected override object ResolveSecondPart(string[] input)
+		{
+			return string.Empty;
+		}
+	}
+}
