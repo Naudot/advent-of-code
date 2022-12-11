c08c71e77fa2dbabcfbd885474bdea6b484ab6d4
nnaudot <nnaudot@manzalab.com>
2022-12-11 16:35:46 +0100

    Clean the code a little bit

--- AdventOfCode/Year2022/DayEleven.cs
+++ AdventOfCode/Year2022/DayEleven.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Year2022
@@ -26,115 +25,19 @@ namespace AdventOfCode.Year2022
 	{
 		protected override object ResolveFirstPart(string[] input)
 		{
-			List<Monkey> monkeys = new List<Monkey>();
-			int monkeyNumber = 8;
-			int monkeyInfo = 7;
-
-			for (int i = 0; i < monkeyNumber; i++)
-			{
-				Monkey monkey = new Monkey();
-				monkey.ID = i;
-
-				for (int j = 0; j < monkeyInfo; j++)
-				{
-					int lineIndex = i * monkeyInfo + j;
-					if (lineIndex >= input.Length)
-					{
-						continue;
-					}
-
-					string line = input[lineIndex];
-
-					if (j == 1)
-					{
-						line = line.Replace("Starting items: ", string.Empty).Trim();
-						monkey.Items = line.Split(',').Select(number => new Item() { WorryValue = int.Parse(number) }).ToList();
-					}
-					if (j == 2)
-					{
-						if (line.Contains("old * old"))
-						{
-							monkey.IsOperationSelfMult = true;
-						}
-						else
-						{
-							monkey.IsOperationSelfMult = false;
-							monkey.IsOperationMult = line.Contains("*");
-							string toParse = line.Replace("Operation: new = old ", string.Empty).Replace("*", string.Empty).Replace("+", string.Empty).Trim();
-							monkey.OperationValue = int.Parse(toParse);
-						}
-					}
-					if (j == 3)
-					{
-						monkey.TestValue = int.Parse(line.Replace("Test: divisible by ", string.Empty).Trim());
-					}
-					if (j == 4)
-					{
-						monkey.TrueMonkeyID = int.Parse(line.Replace("If true: throw to monkey ", string.Empty).Trim());
-					}
-					if (j == 5)
-					{
-						monkey.FalseMonkeyID = int.Parse(line.Replace("If false: throw to monkey ", string.Empty).Trim());
-					}
-				}
-
-				monkeys.Add(monkey);
-			}
-
-			int turnCount = 20;
-
-			for (int i = 0; i < turnCount; i++)
-			{
-				for (int j = 0; j < monkeys.Count; j++)
-				{
-					Monkey monkey = monkeys[j];
-
-					for (int k = 0; k < monkey.Items.Count; k++)
-					{
-						monkey.InspectedItems++;
-
-						Item item = monkey.Items[k];
-
-						if (monkey.IsOperationSelfMult)
-						{
-							item.WorryValue = item.WorryValue * item.WorryValue;
-						}
-						else if (monkey.IsOperationMult)
-						{
-							item.WorryValue = item.WorryValue * monkey.OperationValue;
-						}
-						else
-						{
-							item.WorryValue = item.WorryValue + monkey.OperationValue;
-						}
-
-						item.WorryValue /= 3;
-
-						if (item.WorryValue % monkey.TestValue == 0)
-						{
-							monkeys.FirstOrDefault(newMonkey => newMonkey.ID == monkey.TrueMonkeyID).Items.Add(item);
-						}
-						else
-						{
-							monkeys.FirstOrDefault(newMonkey => newMonkey.ID == monkey.FalseMonkeyID).Items.Add(item);
-						}
-					}
-
-					monkey.Items.Clear();
-				}
-			}
-
-			monkeys = monkeys.OrderByDescending(monkey => monkey.InspectedItems).ToList();
-
-			return monkeys[0].InspectedItems * monkeys[1].InspectedItems;
+			return GetValue(input, false);
 		}
 
 		protected override object ResolveSecondPart(string[] input)
 		{
+			return GetValue(input, true);
+		}
+
+		public long GetValue(string[] input, bool isSecondPart)
+		{
 			List<Monkey> monkeys = new List<Monkey>();
 			int monkeyNumber = 8;
 			int monkeyInfo = 7;
-
 			int commonWorryValue = 1;
 
 			for (int i = 0; i < monkeyNumber; i++)
@@ -174,8 +77,10 @@ namespace AdventOfCode.Year2022
 					if (j == 3)
 					{
 						monkey.TestValue = int.Parse(line.Replace("Test: divisible by ", string.Empty).Trim());
-
-						commonWorryValue *= monkey.TestValue;
+						if (isSecondPart)
+						{
+							commonWorryValue *= monkey.TestValue;
+						}
 					}
 					if (j == 4)
 					{
@@ -190,7 +95,7 @@ namespace AdventOfCode.Year2022
 				monkeys.Add(monkey);
 			}
 
-			int turnCount = 10000;
+			int turnCount = isSecondPart ? 10000 : 20;
 
 			for (int i = 0; i < turnCount; i++)
 			{
@@ -217,7 +122,14 @@ namespace AdventOfCode.Year2022
 							item.WorryValue = item.WorryValue + monkey.OperationValue;
 						}
 
-						item.WorryValue %= commonWorryValue;
+						if (!isSecondPart)
+						{
+							item.WorryValue /= 3;
+						}
+						else
+						{
+							item.WorryValue %= commonWorryValue;
+						}
 
 						if (item.WorryValue % monkey.TestValue == 0)
 						{
