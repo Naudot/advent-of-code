ba7009f3723642af041e0614d322a86d8c169559
Nicolas Naudot <nicolas.naudot@on-x.com>
2023-12-07 11:49:11 +0100

    Begins part 2 of day 7

--- AdventOfCode/Year2023/DaySeven.cs
+++ AdventOfCode/Year2023/DaySeven.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode.Year2023
 {
 	public class DaySeven : Day2023
 	{
-		private char[] cards = new[]
+		private char[] firstPartCards = new[]
 		{
 			'2',
 			'3',
@@ -22,31 +22,54 @@ namespace AdventOfCode.Year2023
 			'K',
 			'A'
 		};
+		
+		private char[] secondPartCards = new[]
+		{
+			'J',
+			'2',
+			'3',
+			'4',
+			'5',
+			'6',
+			'7',
+			'8',
+			'9',
+			'T',
+			'Q',
+			'K',
+			'A'
+		};
 
 		private int[] values = new int[]
 		{
-			537824 * 7, // Five of a kind
-			537824 * 6, // Four of a kind
-			537824 * 5, // Full house
-			537824 * 4, // Three of a kind
-			537824 * 3, // Two pairs
-			537824 * 2, // One paire
+			537824 * 6, // Five of a kind
+			537824 * 5, // Four of a kind
+			537824 * 4, // Full house
+			537824 * 3, // Three of a kind
+			537824 * 2, // Two pairs
+			537824 * 1, // One paire
 			0 // High card
 		};
 
 		protected override object ResolveFirstPart(string[] input)
 		{
-			List<(int, int, string)> valuesOfDecks = new List<(int, int, string)>();
+			return ResolveDay(input, false, firstPartCards);
+		}
+
+		protected override object ResolveSecondPart(string[] input)
+		{
+			return ResolveDay(input, true, secondPartCards);
+		}
+
+		private object ResolveDay(string[] input, bool useJocker, char[] cardReferences)
+		{
+			List<(int, int)> valuesOfDecks = new List<(int, int)>();
 
 			for (int i = 0; i < input.Length; i++)
 			{
 				string cardsDeck = input[i].Split(' ')[0];
 				int value = int.Parse(input[i].Split(' ')[1]);
-
-				int cardsTypeValue = GetValueOfType(cardsDeck);
-				int cardsValue = GetValueOfCards(cardsDeck);
-
-				valuesOfDecks.Add((cardsTypeValue + cardsValue, value, cardsDeck));
+				valuesOfDecks.Add((GetValueOfType(cardsDeck, useJocker) + GetValueOfCards(cardReferences, cardsDeck), value));
 			}
 
 			valuesOfDecks = valuesOfDecks.OrderBy(item => item.Item1).ToList();
@@ -61,21 +84,43 @@ namespace AdventOfCode.Year2023
 			return finalValue;
 		}
 
-		protected override object ResolveSecondPart(string[] input)
-		{
-			return string.Empty;
-		}
-
-		public int GetValueOfType(string cardsDeck)
+		// 253840749 too high 
+		// 253216559 too low
+		// 253296336 too low
+		// 253609148 not right
+		public int GetValueOfType(string cardsDeck, bool useJocker)
 		{
 			Dictionary<char, int> charCount = new Dictionary<char, int>();
 
 			for (int i = 0; i < cardsDeck.Length; i++)
 			{
-				if (charCount.ContainsKey(cardsDeck[i]))
-					charCount[cardsDeck[i]]++;
+				if (useJocker)
+				{
+					if (cardsDeck[i] == 'J')
+					{
+						for (int j = 0; j < secondPartCards.Length; j++)
+						{
+							if (charCount.ContainsKey(secondPartCards[j]))
+								charCount[secondPartCards[j]]++;
+							else
+								charCount.Add(secondPartCards[j], 1);
+						}
+					}
+					else
+					{
+						if (charCount.ContainsKey(cardsDeck[i]))
+							charCount[cardsDeck[i]]++;
+						else
+							charCount.Add(cardsDeck[i], 1);
+					}
+				}
 				else
-					charCount.Add(cardsDeck[i], 1);
+				{
+					if (charCount.ContainsKey(cardsDeck[i]))
+						charCount[cardsDeck[i]]++;
+					else
+						charCount.Add(cardsDeck[i], 1);
+				}
 			}
 
 			if (charCount.Where(pair => pair.Value == 5).Any())
@@ -86,11 +131,11 @@ namespace AdventOfCode.Year2023
 			{
 				return values[1];
 			}
-			if (charCount.Where(pair => pair.Value == 3).Any() && charCount.Where(pair => pair.Value == 2).Any())
+			if (charCount.Where(pair => pair.Value == 3).Count() == 1 && charCount.Where(pair => pair.Value == 2).Any())
 			{
 				return values[2];
 			}
-			if (charCount.Where(pair => pair.Value == 3).Any() && !charCount.Where(pair => pair.Value == 2).Any())
+			if (charCount.Where(pair => pair.Value == 3).Count() == 1 && !charCount.Where(pair => pair.Value == 2).Any())
 			{
 				return values[3];
 			}
@@ -105,14 +150,14 @@ namespace AdventOfCode.Year2023
 			return values[6];
 		}
 
-		public int GetValueOfCards(string cardsDeck)
+		public int GetValueOfCards(char[] cardReferences, string cardsDeck)
 		{
 			int value = 0;
 
 			for (int i = 0; i < cardsDeck.Length; i++)
 			{
 				int pow = (int)Math.Pow(14, cardsDeck.Length - 1 - i);
-				value += Array.IndexOf(cards, cardsDeck[i]) * pow;
+				value += Array.IndexOf(cardReferences, cardsDeck[i]) * pow;
 			}
 
 			return value;
