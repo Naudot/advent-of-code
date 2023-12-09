304c8b5f78fe654307bf652caa37743248b9f9c2
nnaudot <nnaudot@manzalab.com>
2023-12-09 20:02:42 +0100

    Clean code and add part two

--- AdventOfCode/Year2023/DayNine.cs
+++ AdventOfCode/Year2023/DayNine.cs
@@ -1,56 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Year2023
 {
 	public class DayNine : Day2023
 	{
-		protected override bool DeactivateJIT
-		{
-			get
-			{
-				return true;
-			}
-		}
-
 		protected override object ResolveFirstPart(string[] input)
 		{
-			int sum = 0;
-
-			for (int i = 0; i < input.Length; i++)
-			{
-				int[] diff;
-				int[] current = input[i].Split(' ').Select(term => int.Parse(term)).ToArray();
-
-				List<int[]> arrays = new List<int[]>();
-				arrays.Add(current);
-
-				bool isFound = false;
-				while (!isFound)
-				{
-					diff = new int[current.Length - 1];
-					for (int j = 0; j < current.Length - 1; j++)
-					{
-						int first = current[j];
-						int second = current[j + 1];
-						diff[j] = second - first;
-					}
-
-					current = diff;
-					arrays.Add(current);
-					isFound = !diff.Where(value => value != 0).Any();
-				}
-
-				sum += arrays.Select(array => array[array.Length - 1]).Sum();
-			}
-
-			return sum;
+			return input.Select(line => GetInterpolationForLine(line, false)).Sum();
 		}
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			return string.Empty;
+			return input.Select(line => GetInterpolationForLine(line, true)).Sum();
+		}
+
+		private int GetInterpolationForLine(string line, bool isBackward)
+		{
+			int[] diff;
+			int[] current = line.Split(' ').Select(term => int.Parse(term)).ToArray();
+
+			List<int[]> arrays = new List<int[]>();
+			arrays.Add(current);
+
+			bool isFound = false;
+			while (!isFound)
+			{
+				diff = new int[current.Length - 1];
+				for (int j = 0; j < current.Length - 1; j++)
+				{
+					int first = current[j];
+					int second = current[j + 1];
+					diff[j] = second - first;
+				}
+
+				current = diff;
+				arrays.Add(current);
+				isFound = !diff.Where(value => value != 0).Any();
+			}
+
+			int interpolatedValue = 0;
+			if (isBackward)
+			{
+				for (int j = 1; j < arrays.Count; j++)
+				{
+					int knownValue = arrays[arrays.Count - 1 - j].First();
+					interpolatedValue = knownValue - interpolatedValue;
+				}
+			}
+			else
+			{
+				interpolatedValue = arrays.Select(array => array.Last()).Sum();
+			}
+
+			return interpolatedValue;
 		}
 	}
 }
