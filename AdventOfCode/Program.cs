c0286b048e76407cbda6dd4dfe0274f9d6505fac
nnaudot <nnaudot@manzalab.com>
2023-12-18 07:58:39 +0100

    Migrate to C# 6.0

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,156 +1,139 @@
-﻿using System;
+﻿using AdventOfCode;
 
-namespace AdventOfCode
-{
-	public class Program
-	{
-		private static void Main(string[] args)
-		{
-			new AdventOfCode();
+//new Year2015.DayOne();
+//new Year2015.DayTwo();
+//new Year2015.DayThree();
+//new Year2015.DayFour();
+//new Year2015.DayFive();
+//new Year2015.DaySix();
+//new Year2015.DaySeven();
+//new Year2015.DayEight();
+//new Year2015.DayNine();
+//new Year2015.DayTen();
+//new Year2015.DayEleven();
 
-			Console.ReadKey();
-		}
-	}
+//new Year2016.DayOne();
 
-	public class AdventOfCode
-	{
-		public AdventOfCode()
-		{
-			//new Year2015.DayOne();
-			//new Year2015.DayTwo();
-			//new Year2015.DayThree();
-			//new Year2015.DayFour();
-			//new Year2015.DayFive();
-			//new Year2015.DaySix();
-			//new Year2015.DaySeven();
-			//new Year2015.DayEight();
-			//new Year2015.DayNine();
-			//new Year2015.DayTen();
-			//new Year2015.DayEleven();
+//new Year2017.DayOne();
 
-			//new Year2016.DayOne();
+//new Year2018.DayOne();
+//new Year2018.DayTwentyThree();
 
-			//new Year2017.DayOne();
+//new Year2019.DayOne();
+//new Year2019.DayTwo();
 
-			//new Year2018.DayOne();
-			//new Year2018.DayTwentyThree();
+//new Year2020.DayOne();
+//new Year2020.DayTwo();
+//new Year2020.DayThree();
+//new Year2020.DayFour();
+//new Year2020.DayFive();
+//new Year2020.DaySix();
+//new Year2020.DaySeven();
+//new Year2020.DayEight();
+//new Year2020.DayNine();
+//new Year2020.DayTen();
+//new Year2020.DayEleven();
+//new Year2020.DayTwelve();
+//new Year2020.DayThirteen();
+//new Year2020.DayFourteen();
+//new Year2020.DayFifteen();
+//new Year2020.DaySixteen();
+//new Year2020.DaySeventeen();
+//new Year2020.DayEighteen();
+//new Year2020.DayNineteen();
+//new Year2020.DayTwenty();
+//new Year2020.DayTwentyOne();
+//new Year2020.DayTwentyTwo();
+//new Year2020.DayTwentyThree();
+//new Year2020.DayTwentyFour();
+//new Year2020.DayTwentyFive();
 
-			//new Year2019.DayOne();
-			//new Year2019.DayTwo();
+//new Year2021.DayOne();
+//new Year2021.DayTwo();
+//new Year2021.DayThree();
+//new Year2021.DayFour();
+//new Year2021.DayFive();
+//new Year2021.DaySix();
+//new Year2021.DaySeven();
+//new Year2021.DayEight();
+//new Year2021.DayNine();
+//new Year2021.DayTen();
+//new Year2021.DayEleven();
+//new Year2021.DayTwelve();
+//new Year2021.DayThirteen();
+//new Year2021.DayFourteen();
+//new Year2021.DayFifteen();
+//new Year2021.DaySixteen();
+//new Year2021.DaySeventeen();
+//new Year2021.DayEighteen();
+//new Year2021.DayNineteen();
+//new Year2021.DayTwenty();
+//new Year2021.DayTwentyOne();
+//new Year2021.DayTwentyTwo();
+//new Year2021.DayTwentyThree();
+//new Year2021.DayTwentyFour();
+//new Year2021.DayTwentyFive();
 
-			//new Year2020.DayOne();
-			//new Year2020.DayTwo();
-			//new Year2020.DayThree();
-			//new Year2020.DayFour();
-			//new Year2020.DayFive();
-			//new Year2020.DaySix();
-			//new Year2020.DaySeven();
-			//new Year2020.DayEight();
-			//new Year2020.DayNine();
-			//new Year2020.DayTen();
-			//new Year2020.DayEleven();
-			//new Year2020.DayTwelve();
-			//new Year2020.DayThirteen();
-			//new Year2020.DayFourteen();
-			//new Year2020.DayFifteen();
-			//new Year2020.DaySixteen();
-			//new Year2020.DaySeventeen();
-			//new Year2020.DayEighteen();
-			//new Year2020.DayNineteen();
-			//new Year2020.DayTwenty();
-			//new Year2020.DayTwentyOne();
-			//new Year2020.DayTwentyTwo();
-			//new Year2020.DayTwentyThree();
-			//new Year2020.DayTwentyFour();
-			//new Year2020.DayTwentyFive();
+//new Year2022.DayOne();
+//new Year2022.DayTwo();
+//new Year2022.DayThree();
+//new Year2022.DayFour();
+//new Year2022.DayFive();
+//new Year2022.DaySix();
+//new Year2022.DaySeven();
+//new Year2022.DayEight();
+//new Year2022.DayNine();
+//new Year2022.DayTen();
+//new Year2022.DayEleven();
+//new Year2022.DayTwelve();
+//new Year2022.DayThirteen();
+//new Year2022.DayFourteen();
+//new Year2022.DayFifteen();
+//new Year2022.DaySixteen();
+//new Year2022.DaySeventeen();
+//new Year2022.DayEighteen();
+//new Year2022.DayNineteen();
+//new Year2022.DayTwenty();
+//new Year2022.DayTwentyOne();
+//new Year2022.DayTwentyTwo();
+//new Year2022.DayTwentyThree();
+//new Year2022.DayTwentyFour();
+//new Year2022.DayTwentyFive();
 
-			//new Year2021.DayOne();
-			//new Year2021.DayTwo();
-			//new Year2021.DayThree();
-			//new Year2021.DayFour();
-			//new Year2021.DayFive();
-			//new Year2021.DaySix();
-			//new Year2021.DaySeven();
-			//new Year2021.DayEight();
-			//new Year2021.DayNine();
-			//new Year2021.DayTen();
-			//new Year2021.DayEleven();
-			//new Year2021.DayTwelve();
-			//new Year2021.DayThirteen();
-			//new Year2021.DayFourteen();
-			//new Year2021.DayFifteen();
-			//new Year2021.DaySixteen();
-			//new Year2021.DaySeventeen();
-			//new Year2021.DayEighteen();
-			//new Year2021.DayNineteen();
-			//new Year2021.DayTwenty();
-			//new Year2021.DayTwentyOne();
-			//new Year2021.DayTwentyTwo();
-			//new Year2021.DayTwentyThree();
-			//new Year2021.DayTwentyFour();
-			//new Year2021.DayTwentyFive();
+//new Year2023.DayOne();
+//new Year2023.DayTwo();
+//new Year2023.DayThree();
+//new Year2023.DayFour();
+//new Year2023.DayFive();
+//new Year2023.DaySix();
+//new Year2023.DaySeven();
+//new Year2023.DayEight();
+//new Year2023.DayNine();
+//new Year2023.DayTen();
+//new Year2023.DayEleven();
+//new Year2023.DayTwelve();
+//new Year2023.DayThirteen();
+//new Year2023.DayFourteen();
+//new Year2023.DayFifteen();
+//new Year2023.DaySixteen();
 
-			//new Year2022.DayOne();
-			//new Year2022.DayTwo();
-			//new Year2022.DayThree();
-			//new Year2022.DayFour();
-			//new Year2022.DayFive();
-			//new Year2022.DaySix();
-			//new Year2022.DaySeven();
-			//new Year2022.DayEight();
-			//new Year2022.DayNine();
-			//new Year2022.DayTen();
-			//new Year2022.DayEleven();
-			//new Year2022.DayTwelve();
-			//new Year2022.DayThirteen();
-			//new Year2022.DayFourteen();
-			//new Year2022.DayFifteen();
-			//new Year2022.DaySixteen();
-			//new Year2022.DaySeventeen();
-			//new Year2022.DayEighteen();
-			//new Year2022.DayNineteen();
-			//new Year2022.DayTwenty();
-			//new Year2022.DayTwentyOne();
-			//new Year2022.DayTwentyTwo();
-			//new Year2022.DayTwentyThree();
-			//new Year2022.DayTwentyFour();
-			//new Year2022.DayTwentyFive();
+new AdventOfCode.Year2023.DaySeventeen();
 
-			//new Year2023.DayOne();
-			//new Year2023.DayTwo();
-			//new Year2023.DayThree();
-			//new Year2023.DayFour();
-			//new Year2023.DayFive();
-			//new Year2023.DaySix();
-			//new Year2023.DaySeven();
-			//new Year2023.DayEight();
-			//new Year2023.DayNine();
-			//new Year2023.DayTen();
-			//new Year2023.DayEleven();
-			//new Year2023.DayTwelve();
-			//new Year2023.DayThirteen();
-			//new Year2023.DayFourteen();
-			//new Year2023.DayFifteen();
-			new Year2023.DaySixteen();
+//Day2015.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2016.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2017.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2018.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2019.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2020.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2021.CalculateAndLogResults();
+//Console.WriteLine();
+//Day2022.CalculateAndLogResults();
+//Console.WriteLine();
 
-			//Day2015.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2016.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2017.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2018.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2019.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2020.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2021.CalculateAndLogResults();
-			//Console.WriteLine();
-			//Day2022.CalculateAndLogResults();
-			//Console.WriteLine();
-
-			Day2023.CalculateAndLogResults();
-		}
-	}
-}
+Day2023.CalculateAndLogResults();
---
+++ AdventOfCode/Year2023/DaySeventeen.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Year2023
+{
+	public class DaySeventeen : Day2023
+	{
+		protected override object ResolveFirstPart(string[] input)
+		{
+			return string.Empty;
+		}
+
+		protected override object ResolveSecondPart(string[] input)
+		{
+			return string.Empty;
+		}
+	}
+}
