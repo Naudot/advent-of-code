a6787cb4fa8918ddcb0a72e2dee21c75b0fd7a55
Nicolas Naudot <nicolas.naudot@on-x.com>
2024-12-02 09:39:04 +0100

    Add part two

--- AdventOfCode/Year2024/DayTwo.cs
+++ AdventOfCode/Year2024/DayTwo.cs
@@ -9,17 +9,7 @@ namespace AdventOfCode.Year2024
 			for (int i = 0; i < input.Length; i++)
 			{
 				int[] levels = input[i].Split(' ').Select(level => int.Parse(level)).ToArray();
-
-				bool isIncreasing = (levels[0] - levels[1]) < 0;
-				for (int j = 0; j < levels.Length - 1; j++)
-				{
-					int diff = levels[j] - levels[j + 1];
-					if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3 || (isIncreasing ? diff > 0 : diff < 0))
-					{
-						safeReportsCount--;
-						break;
-					}
-				}
+				safeReportsCount -= IsReportSafe(levels) ? 0 : 1;
 			}
 
 			return safeReportsCount;
@@ -27,7 +17,41 @@ namespace AdventOfCode.Year2024
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			return 0;
+			int safeReportsCount = input.Length;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				int[] levels = input[i].Split(' ').Select(level => int.Parse(level)).ToArray();
+				bool isSafe = IsReportSafe(levels);
+
+				if (!isSafe)
+				{
+					bool isOneReportSafeAmongTrimmedLevels = false;
+					for (int j = 0; j < levels.Length; j++)
+					{
+						int[] trimmedLevels = levels.Where((val, index) => index != j).ToArray();
+						isOneReportSafeAmongTrimmedLevels |= IsReportSafe(trimmedLevels);
+					}
+
+					if (!isOneReportSafeAmongTrimmedLevels)
+						safeReportsCount --;
+				}
+			}
+
+			return safeReportsCount;
+		}
+
+		private bool IsReportSafe(int[] levels)
+		{
+			bool isIncreasing = (levels[0] - levels[1]) < 0;
+			for (int j = 0; j < levels.Length - 1; j++)
+			{
+				int diff = levels[j] - levels[j + 1];
+				if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3 || (isIncreasing ? diff > 0 : diff < 0))
+					return false;
+			}
+
+			return true;
 		}
 	}
 }
