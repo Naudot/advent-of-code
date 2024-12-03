bb30ed8002fea0ad80e6fc98a91c4e547370a48b
Nicolas Naudot <nicolas.naudot@on-x.com>
2024-12-03 10:28:29 +0100

    Add d3 p2

--- AdventOfCode/Year2024/DayThree.cs
+++ AdventOfCode/Year2024/DayThree.cs
@@ -12,11 +12,7 @@ namespace AdventOfCode.Year2024
 			{
 				MatchCollection matches = Regex.Matches(input[i], @"mul\((\d{1,3}),(\d{1,3})\)");
 				for (int j = 0; j < matches.Count; j++)
-				{
-					int left = int.Parse(matches[j].Groups[1].Value);
-					int right = int.Parse(matches[j].Groups[2].Value);
-					result += left * right;
-				}
+					result += int.Parse(matches[j].Groups[1].Value) * int.Parse(matches[j].Groups[2].Value);
 			}
 
 			return result;
@@ -24,7 +20,22 @@ namespace AdventOfCode.Year2024
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			return 0;
+			double result = 0;
+			bool enabled = true;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				MatchCollection matches = Regex.Matches(input[i], @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)");
+				for (int j = 0; j < matches.Count; j++)
+				{
+					if (matches[j].Value == "do()" || matches[j].Value == "don't()")
+						enabled = matches[j].Value == "do()";
+					else if (enabled)
+						result += int.Parse(matches[j].Groups[1].Value) * int.Parse(matches[j].Groups[2].Value);
+				}
+			}
+
+			return result;
 		}
 	}
 }
