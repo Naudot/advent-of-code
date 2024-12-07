4d3630b8e339a5f045e3a83bcab763147dc36806
nnaudot <naudot.nicolas@gmail.com>
2024-12-07 10:30:30 +0100

    Begin P2

--- AdventOfCode/Year2024/DaySeven.cs
+++ AdventOfCode/Year2024/DaySeven.cs
@@ -5,7 +5,8 @@ namespace AdventOfCode.Year2024
 		private enum Ope
 		{
 			MULT,
-			ADD
+			ADD,
+			CONCAT
 		}
 
 		protected override bool DeactivateJIT
@@ -27,24 +28,50 @@ namespace AdventOfCode.Year2024
 				string[] values = line.Split(':');
 				double sum = double.Parse(values[0]);
 				double[] operators = values[1].Split(' ').Where(val => val != string.Empty).Select(val => double.Parse(val)).ToArray();
+
 				Ope[] operationsRight = new Ope[operators.Length - 1];
-				isReaching |= IsReaching(sum, operators, operationsRight, 0);
+				isReaching |= IsReaching(sum, operators, operationsRight, 0, false);
 				Ope[] operationsLeft = new Ope[operators.Length - 1];
 				operationsLeft[0] = Ope.ADD;
-				isReaching |= IsReaching(sum, operators, operationsLeft, 0);
+				isReaching |= IsReaching(sum, operators, operationsLeft, 0, false);
 
 				result += isReaching ? sum : 0;
-
-				Console.WriteLine($"Found {isReaching} for {sum} at {i}");
 			}
 
 			return result;
 		}
 
-		private bool IsReaching(double sum, double[] operators, Ope[] operations, int depthIndex)
+		protected override object ResolveSecondPart(string[] input)
 		{
-			Log(operations, depthIndex);
+			double result = 0;
 
+			for (int i = 0; i < input.Length; i++)
+			{
+				bool isReaching = false;
+				string line = input[i];
+				string[] values = line.Split(':');
+				double sum = double.Parse(values[0]);
+				double[] operators = values[1].Split(' ').Where(val => val != string.Empty).Select(val => double.Parse(val)).ToArray();
+
+				Ope[] operationsRight = new Ope[operators.Length - 1];
+				isReaching |= IsReaching(sum, operators, operationsRight, 0, true);
+
+				Ope[] operationsMid = new Ope[operators.Length - 1];
+				operationsMid[0] = Ope.ADD;
+				isReaching |= IsReaching(sum, operators, operationsMid, 0, true);
+
+				Ope[] operationsLeft = new Ope[operators.Length - 1];
+				operationsLeft[0] = Ope.CONCAT;
+				isReaching |= IsReaching(sum, operators, operationsLeft, 0, true);
+
+				result += isReaching ? sum : 0;
+			}
+
+			return result;
+		}
+
+		private bool IsReaching(double sum, double[] operators, Ope[] operations, int depthIndex, bool secondPart)
+		{
 			double result = operators[0];
 
 			for (int i = 0; i < operations.Length; i++)
@@ -55,10 +82,19 @@ namespace AdventOfCode.Year2024
 					result *= operators[i + 1];
 				if (ope == Ope.ADD)
 					result += operators[i + 1];
-				if (result == sum)
-					return true;
+				if (ope == Ope.CONCAT && secondPart)
+				{
+					int digit = CountDigits(operators[i + 1]);
+					result *= Math.Pow(10, digit);
+					result += operators[i + 1];
+				}
 
-				// Trouver d'autres cas d'arrêts ?
+				if (result == sum && secondPart)
+				{
+					Log(sum, operators, operations);
+					return true;
+				}
+
 				if (result > sum)
 					break;
 			}
@@ -68,34 +104,47 @@ namespace AdventOfCode.Year2024
 				return false;
 
 			// We have two Ops
-			for (int i = 0; i < 2; i++)
+			for (int i = 0; i < (secondPart ? 3 : 2); i++)
 			{
 				Ope[] newOperations = new Ope[operations.Length];
 				Array.Copy(operations, newOperations, operations.Length);
 				newOperations[newDepth] = (Ope)i;
 
-				if (IsReaching(sum, operators, newOperations, newDepth))
+				if (IsReaching(sum, operators, newOperations, newDepth, secondPart))
 					return true;
 			}
 
 			return false;
 		}
 
-		protected override object ResolveSecondPart(string[] input)
+		public static int CountDigits(double num)
 		{
-			return 0;
+			int digits = 0;
+			while (num >= 1)
+			{
+				digits++;
+				num /= 10;
+			}
+			return digits;
 		}
 
-		private void Log(Ope[] operations, int depthIndex)
+		private static void Log(double sum, double[] operators, Ope[] operations)
 		{
 			string log = string.Empty;
-			for (int i = 0; i < operations.Length; i++)
+
+			for (int i = 0; i < operators.Length; i++)
 			{
-				log += operations[i] == Ope.MULT ? "*" : "+";
-				log += " ";
+				log += operators[i].ToString();
+
+				if (i < operations.Length)
+				{
+					log += " ";
+					log += operations[i] == Ope.MULT ? "*" : (operations[i] == Ope.ADD ? "+" : "||");
+					log += " ";
+				}
 			}
 
-			Console.WriteLine($"Processing {log} at DI {depthIndex}");
+			Console.WriteLine($"Processing {sum} \t \t {log}");
 		}
 	}
 }
