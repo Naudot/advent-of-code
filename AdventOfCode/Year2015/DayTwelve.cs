55065b81e17c09e219b689d64dcd0c3cad10d627
Nicolas Naudot <nicolas.naudot@on-x.com>
2024-12-10 17:22:10 +0100

    Add D12 P1 2015

---
+++ AdventOfCode/Year2015/DayTwelve.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2015
+{
+	public class DayTwelve : Day2015
+	{
+		protected override object ResolveFirstPart(string[] input)
+		{
+			return Regex.Matches(input[0], @"\d+|-\d+").Sum(match => int.Parse(match.Value));
+		}
+
+		protected override object ResolveSecondPart(string[] input)
+		{
+			return 0;
+		}
+	}
+}
