4ddae09350eee37f13a643e592c2d9a3d1e6d8f1
nnaudot <naudot.nicolas@gmail.com>
2024-12-13 12:43:07 +0100

    Begin P2

---
+++ AdventOfCode/Utils/ExtensionsDouble.cs
@@ -0,0 +1,7 @@
+﻿public static class ExtensionsDouble
+{
+	public static bool HasDecimal(this double d)
+	{
+		return Math.Abs(d % 1) >= double.Epsilon;
+	}
+}
--- AdventOfCode/Year2024/DayThirteen.cs
+++ AdventOfCode/Year2024/DayThirteen.cs
@@ -14,9 +14,9 @@ namespace AdventOfCode.Year2024
 
 		protected override object ResolveFirstPart(string[] input)
 		{
-			long sum = 0;
+			double sum = 0;
 
-			for (long i = 0; i < input.Length; i += 4)
+			for (int i = 0; i < input.Length; i += 4)
 			{
 				if (i + 2 >= input.Length)
 					break;
@@ -25,9 +25,9 @@ namespace AdventOfCode.Year2024
 				Match matchB = Regex.Match(input[i + 1], @"Button B: X\+(\d+), Y\+(\d+)");
 				Match matchPrize = Regex.Match(input[i + 2], @"Prize: X=(\d+), Y=(\d+)");
 
-				(long x, long y) a = (long.Parse(matchA.Groups[1].Value), long.Parse(matchA.Groups[2].Value));
-				(long x, long y) b = (long.Parse(matchB.Groups[1].Value), long.Parse(matchB.Groups[2].Value));
-				(long x, long y) prize = (long.Parse(matchPrize.Groups[1].Value), long.Parse(matchPrize.Groups[2].Value));
+				(double x, double y) a = (double.Parse(matchA.Groups[1].Value), double.Parse(matchA.Groups[2].Value));
+				(double x, double y) b = (double.Parse(matchB.Groups[1].Value), double.Parse(matchB.Groups[2].Value));
+				(double x, double y) prize = (double.Parse(matchPrize.Groups[1].Value), double.Parse(matchPrize.Groups[2].Value));
 
 				sum += GetPerfectInputValue(a, b, prize, false);
 			}
@@ -37,45 +37,58 @@ namespace AdventOfCode.Year2024
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			long sum = 0;
+			double sum = 0;
 
-			//for (long i = 0; i < input.Length; i += 4)
-			//{
-			//	if (i + 2 >= input.Length)
-			//		break;
+			for (int i = 0; i < input.Length; i += 4)
+			{
+				if (i + 2 >= input.Length)
+					break;
 
-			//	Match matchA = Regex.Match(input[i], @"Button A: X\+(\d+), Y\+(\d+)");
-			//	Match matchB = Regex.Match(input[i + 1], @"Button B: X\+(\d+), Y\+(\d+)");
-			//	Match matchPrize = Regex.Match(input[i + 2], @"Prize: X=(\d+), Y=(\d+)");
+				Match matchA = Regex.Match(input[i], @"Button A: X\+(\d+), Y\+(\d+)");
+				Match matchB = Regex.Match(input[i + 1], @"Button B: X\+(\d+), Y\+(\d+)");
+				Match matchPrize = Regex.Match(input[i + 2], @"Prize: X=(\d+), Y=(\d+)");
 
-			//	(long x, long y) a = (long.Parse(matchA.Groups[1].Value), long.Parse(matchA.Groups[2].Value));
-			//	(long x, long y) b = (long.Parse(matchB.Groups[1].Value), long.Parse(matchB.Groups[2].Value));
-			//	(long x, long y) prize = (long.Parse(matchPrize.Groups[1].Value), long.Parse(matchPrize.Groups[2].Value));
+				(double x, double y) a = (double.Parse(matchA.Groups[1].Value), double.Parse(matchA.Groups[2].Value));
+				(double x, double y) b = (double.Parse(matchB.Groups[1].Value), double.Parse(matchB.Groups[2].Value));
+				(double x, double y) prize = (double.Parse(matchPrize.Groups[1].Value), double.Parse(matchPrize.Groups[2].Value));
 
-			//	sum += GetPerfectInputValue(a, b, prize, true);
-			//}
+				sum += GetPerfectInputValue(a, b, prize, true);
+			}
 
 			return sum;
 		}
 
-		private long GetPerfectInputValue((long x, long y) a, (long x, long y) b, (long x, long y) prize, bool isSecondPart)
+		private double GetPerfectInputValue((double x, double y) a, (double x, double y) b, (double x, double y) prize, bool isSecondPart)
 		{
-			//if (isSecondPart)
-			//	prize = (prize.x + 10000000000000, prize.y + 10000000000000);
+			if (isSecondPart)
+				prize = (prize.x + 10000000000000, prize.y + 10000000000000);
 
-			for (long pushA = 0; pushA < 100; pushA++)
-			{
-				for (long pushB = 0; pushB < 100; pushB++)
-				{
-					long finalX = a.x * pushA + b.x * pushB;
-					long finalY = a.y * pushA + b.y * pushB;
+			double pushA = ((-1 * (b.y/b.x) * prize.x) + prize.y) / (a.y - (b.y / b.x * a.x));
+			double pushB = (prize.x - a.x * pushA) / b.x;
 
-					if (prize == (finalX, finalY))
-						return pushA * 3 + pushB;
-				}
-			}
+			if (Math.Round(pushA, 10).HasDecimal() || Math.Round(pushB, 10).HasDecimal())
+				return 0;
 
-			return 0;
+			Console.WriteLine(pushA + ", " + pushB);
+			return pushA * 3 + pushB;
+
+			/*
+				Button A: X+94, Y+34
+				Button B: X+22, Y+67
+				Prize: X=8400, Y=5400
+
+				8400 = 94x + 22y avec x le nombre de push de A et y le nombre de push de B
+				5400 = 34x + 67y
+			 
+				y = (8400 - 94x) / 22
+
+				5400 = 34x + 67/22*(8400 - 94x)
+				5400 = 34x + 67/22*8400 - 67/22*94x
+				-67/22*8400 + 5400 = x * (34 - 67/22*94) 
+				-67/22*8400 + 5400 / (34 - 64/22*94) = x
+
+				
+			 */
 		}
 	}
 }
