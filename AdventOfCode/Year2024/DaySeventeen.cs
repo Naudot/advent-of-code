64bc0060481d49c883cd06f577564c52d628248b
Nicolas Naudot <nicolas.naudot@on-x.com>
2024-12-17 13:54:36 +0100

    Bruteforce not working

--- AdventOfCode/Year2024/DaySeventeen.cs
+++ AdventOfCode/Year2024/DaySeventeen.cs
@@ -2,18 +2,61 @@
 {
 	public class DaySeventeen : Day2024
 	{
+		protected override bool DeactivateJIT => true;
+
 		protected override object ResolveFirstPart(string[] input)
 		{
 			int regA = int.Parse(input[0].Split(':')[1].Trim());
 			int regB = int.Parse(input[1].Split(':')[1].Trim());
 			int regC = int.Parse(input[2].Split(':')[1].Trim());
-
 			List<int> program = input[4].Split(':')[1].Trim().Split(',').Select(val => val[0] - '0').ToList();
 
-			int pointer = 0;
-			string log = string.Empty;
+			List<int> outProgram = GetOutput(input, regA, regB, regC, program, -1);
 
-			while (pointer < program.Count)
+			string log = string.Empty;
+			for (int i = 0; i < outProgram.Count; i++)
+				log += outProgram[i].ToString() + ",";
+			return log.Substring(0, log.Length - 1);
+		}
+
+		protected override object ResolveSecondPart(string[] input)
+		{
+			int regA = 0;
+			int regB = int.Parse(input[1].Split(':')[1].Trim());
+			int regC = int.Parse(input[2].Split(':')[1].Trim());
+			List<int> program = input[4].Split(':')[1].Trim().Split(',').Select(val => val[0] - '0').ToList();
+
+			List<int> outProgram = GetOutput(input, regA, regB, regC, program, program.Count);
+
+			//while (!AreListEquals(outProgram, program))
+			//{
+			//	outProgram = GetOutput(input, regA, regB, regC, program, program.Count);
+			//	regA++;
+			//}
+
+			return regA;
+		}
+
+		private bool AreListEquals(List<int> first, List<int> second)
+		{
+			if (first.Count != second.Count)
+				return false;
+
+			for (int i = 0; i < first.Count; i++)
+				if (first[i] != second[i])
+					return false;
+
+			return true;
+		}
+
+		private List<int> GetOutput(string[] input, int regAStartValue, int regB, int regC, List<int> program, int maxOutputCount)
+		{
+			int regA = regAStartValue;
+
+			int pointer = 0;
+			List<int> outProgram = new();
+
+			while (pointer > -1 && pointer < program.Count)
 			{
 				int opCode = program[pointer];
 				int operand = program[pointer + 1];
@@ -57,7 +100,10 @@
 				if (opCode == 5)
 				{
 					int moduled = comboOperand % 8;
-					log += moduled + ",";
+					outProgram.Add(moduled);
+
+					if (maxOutputCount != -1 && outProgram.Count > maxOutputCount)
+						break;
 				}
 				// bdv
 				if (opCode == 6)
@@ -80,12 +126,7 @@
 					pointer += 2;
 			}
 
-			return log.Substring(0, log.Length - 1);
-		}
-
-		protected override object ResolveSecondPart(string[] input)
-		{
-			return 0;
+			return outProgram;
 		}
 
 		private int GetComboOperandValue(int operand, int regA, int regB, int regC)
