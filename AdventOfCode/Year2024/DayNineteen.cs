1a762a0e4a31f9a89a0d8c6097067035a77b7abc
nnaudot <naudot.nicolas@gmail.com>
2024-12-20 00:05:54 +0100

    Add P1 of D19

--- AdventOfCode/Year2024/DayNineteen.cs
+++ AdventOfCode/Year2024/DayNineteen.cs
@@ -7,12 +7,13 @@
 		protected override object ResolveFirstPart(string[] input)
 		{
 			HashSet<string> towels = input[0].Split(", ").ToHashSet();
+			int maxTowelSize = towels.Select(towel => towel.Length).Max();
 			int count = 0;
 
 			for (int i = 2; i < input.Length; i++)
 			{
 				string pattern = input[i];
-				if (GetMaxPointer(towels, pattern, 0) == pattern.Length)
+				if (GetMaxPointer(towels, pattern, 0, maxTowelSize) == pattern.Length)
 					count++;
 			}
 
@@ -24,19 +25,28 @@
 			return 0;
 		}
 
-		private int GetMaxPointer(HashSet<string> towels, string pattern, int pointer)
+		private int GetMaxPointer(HashSet<string> towels, string pattern, int pointer, int maxTowelSize)
 		{
+			//Console.WriteLine("Checking " + pattern.Substring(0, pointer) + " with pointer " + pointer);
+
 			int maxPointer = pointer;
 
 			for (int j = pointer; j < pattern.Length; j++)
 			{
 				int size = j - pointer + 1;
 				string sub = pattern.Substring(pointer, size);
+
+				if (size > maxTowelSize)
+					break;
+
 				if (towels.Contains(sub))
 				{
-					int recursivePointer = GetMaxPointer(towels, pattern, pointer + size);
+					int recursivePointer = GetMaxPointer(towels, pattern, pointer + size, maxTowelSize);
+
 					if (maxPointer < recursivePointer)
 						maxPointer = recursivePointer;
+					if (maxPointer == pattern.Length)
+						return maxPointer;
 				}
 			}
 
