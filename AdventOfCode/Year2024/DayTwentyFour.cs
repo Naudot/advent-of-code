6555b6debbd74768143cd3bf805ff7b806c7700e
nnaudot <naudot.nicolas@gmail.com>
2024-12-24 14:43:24 +0100

    Add D24 P2

--- AdventOfCode/Year2024/DayTwentyFour.cs
+++ AdventOfCode/Year2024/DayTwentyFour.cs
@@ -16,13 +16,28 @@
 			HashSet<(string firstDoor, string secondDoor, string dest, int opCode)> operationsLeft = new();
 			List<(string firstDoor, string secondDoor, string dest, int opCode)> processables = new();
 
+			// Sort for easier logging for P2
+			Dictionary<string, int> xValues = new();
+			Dictionary<string, int> yValues = new();
+
 			for (int i = 0; i < 90; i++)
 			{
 				string[] line = input[i].Replace(" ", string.Empty).Split(':');
 				string door = line[0];
 				int val = int.Parse(line[1]);
 				doorValues.Add(door, val);
+
+				if (door.Contains('x'))
+					xValues.Add(door, val);
+				if (door.Contains('y'))
+					yValues.Add(door, val);
 			}
+
+			// Logging for P2
+			long xVal = GetValueFromBinaryDic(xValues);
+			long yVal = GetValueFromBinaryDic(yValues);
+			long wantedValue = xVal + yVal;
+			Console.WriteLine("Wanted value \t" + wantedValue);
 
 			for (int i = 91; i < input.Length; i++)
 			{
@@ -49,8 +64,18 @@
 				line = line.Replace(" ->", string.Empty);
 
 				string[] doors = line.Split(' ');
-				inputOp.firstDoor = doors[0];
-				inputOp.secondDoor = doors[1];
+
+				if (doors[0].Contains('x'))
+				{
+					inputOp.firstDoor = doors[0];
+					inputOp.secondDoor = doors[1];
+				}
+				else
+				{
+					inputOp.firstDoor = doors[1];
+					inputOp.secondDoor = doors[0];
+				}
+
 				inputOp.dest = doors[2];
 
 				if (doorValues.ContainsKey(inputOp.firstDoor) && doorValues.ContainsKey(inputOp.secondDoor))
@@ -59,14 +84,19 @@
 					operationsLeft.Add(inputOp);
 			}
 
+			processables.Sort();
+
 			while (processables.Count != 0)
 			{
+				Console.WriteLine("Nouvelle itération");
 				for (int i = 0; i < processables.Count; i++)
 				{
 					string firstDoor = processables[i].firstDoor;
 					string secondDoor = processables[i].secondDoor;
 					string dest = processables[i].dest;
 					int opCode = processables[i].opCode; // 1 AND, 2 OR, 3 XOR
+
+					Console.WriteLine("Processing : " + firstDoor + " " + (opCode == 1 ? "AND" : opCode == 2 ? "OR" : "XOR") + " " + secondDoor + " -> " + dest);
 
 					int firstDoorValue = doorValues[firstDoor];
 					int secondDoorValue = doorValues[secondDoor];
@@ -94,21 +124,30 @@
 					operationsLeft.Remove(processables[i]);
 			}
 
-			List<(string, int)> zs = doorValues
+			long result = GetValueFromBinaryDic(doorValues
 				.Where(pair => pair.Key.Contains('z'))
-				.Select(pair => (pair.Key, pair.Value))
-				.OrderByDescending(z => z.Key)
-				.ToList();
+				.ToDictionary(pair => pair.Key, pair => pair.Value));
 
-			string binaryString = string.Empty;
-			for (int i = 0; i < zs.Count; i++)
-				binaryString += zs[i].Item2;
-			return Convert.ToInt64(binaryString, 2);
+			return result;
 		}
 
 		protected override object ResolveSecondPart(string[] input)
 		{
-			return 0;
+			// J'ai tout regardé à la main grâce aux Console.WriteLine() de la partie 1 :)
+			return "dgr,dtv,fgc,mtj,vvm,z12,z29,z37";
+		}
+
+		private long GetValueFromBinaryDic(Dictionary<string, int> values)
+		{
+			List<int> orderedVal = values
+				.OrderByDescending(val => val.Key)
+				.Select(pair => pair.Value)
+				.ToList();
+
+			string binaryString = string.Empty;
+			for (int i = 0; i < orderedVal.Count; i++)
+				binaryString += orderedVal[i];
+			return Convert.ToInt64(binaryString, 2);
 		}
 	}
 }
