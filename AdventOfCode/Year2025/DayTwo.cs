6f62baac1b6c465cafe0338e7fe2588b8829ee3a
nnaudot <naudot.nicolas@gmail.com>
2025-12-02 22:49:07 +0100

    Clean code

--- AdventOfCode/Year2025/DayTwo.cs
+++ AdventOfCode/Year2025/DayTwo.cs
@@ -2,25 +2,6 @@
 {
 	public class DayTwo : Day2025
 	{
-		long[] pows =
-		{
-			0,
-			1,
-			10,
-			100,
-			1000,
-			10000,
-			100000,
-			1000000,
-			10000000,
-			100000000,
-			1000000000,
-			10000000000,
-			100000000000,
-			1000000000000,
-			10000000000000
-		};
-
 		protected override object ResolveFirstPart(string[] input)
 		{
 			string[] ranges = input[0].Split(',');
