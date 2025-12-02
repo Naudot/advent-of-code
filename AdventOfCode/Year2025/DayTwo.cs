7ff35f84ea7689ee1789d975f755467a94953b1d
nnaudot <naudot.nicolas@gmail.com>
2025-12-02 20:10:30 +0100

    Add 2025 D2 P2

--- AdventOfCode/Year2025/DayTwo.cs
+++ AdventOfCode/Year2025/DayTwo.cs
@@ -1,7 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
-namespace AdventOfCode.Year2025
+﻿namespace AdventOfCode.Year2025
 {
 	public class DayTwo : Day2025
 	{
@@ -43,6 +40,18 @@ namespace AdventOfCode.Year2025
 
 				for (long j = min; j <= max; j++)
 				{
+					if (IsInvalidCut(j, 2))
+						invalidIDsSum += j;
+					else if (IsInvalidCut(j, 3))
+						invalidIDsSum += j;
+					else if (IsInvalidCut(j, 4))
+						invalidIDsSum += j;
+					else if (IsInvalidCut(j, 5))
+						invalidIDsSum += j;
+					else if (IsInvalidCut(j, 6))
+						invalidIDsSum += j;
+					else if (IsInvalidCut(j, 7))
+						invalidIDsSum += j;
 				}
 			}
 
@@ -53,16 +62,39 @@ namespace AdventOfCode.Year2025
 		{
 			int numberOfDigits = number.CountDigits();
 
-			if (numberOfDigits % 2 == 1)
+			if (numberOfDigits % 2 != 0)
 				return false;
 
-			for (int j = 0; j < numberOfDigits / 2; j++)
+			for (int i = 0; i < numberOfDigits / 2; i++)
 			{
-				long firstDigitFromTheCenter = (number / (int)Math.Pow(10, numberOfDigits / 2 + j)) % 10;
-				long firstDigitFromTheRight = (number / (int)Math.Pow(10, j)) % 10;
+				long firstDigitFromTheCenter = (number / (int)Math.Pow(10, numberOfDigits / 2 + i)) % 10;
+				long firstDigitFromTheRight = (number / (int)Math.Pow(10, i)) % 10;
 
 				if (firstDigitFromTheCenter != firstDigitFromTheRight)
 					return false;
+			}
+
+			return true;
+		}
+
+		private bool IsInvalidCut(long number, int cut)
+		{
+			int numberOfDigits = number.CountDigits();
+
+			if (numberOfDigits % cut != 0)
+				return false;
+
+			for (int i = 0; i < numberOfDigits / cut; i++)
+			{
+				long[] numbers = new long[cut];
+
+				for (int j = 0; j < cut; j++)
+					numbers[j] = (number / (int)Math.Pow(10, (numberOfDigits / cut) * j + i)) % 10;
+
+				for (int j = 0; j < cut - 1; j++)
+					if (numbers[j] != numbers[j + 1])
+						return false;
+
 			}
 
 			return true;
